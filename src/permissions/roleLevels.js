2bae073c7361c0fe1203b768b4127506d2faf7e1
LUKAI-AI <nghienduc8@gmail.com>
2026-01-09 21:04:30 +0700

    Add files via upload

---
+++ src/permissions/roleLevels.js
@@ -0,0 +1,12 @@
+export const ROLE_LEVEL = {
+  member: 1,
+  manager: 2,
+  admin: 3
+};
+
+// ROLE ID THỰC TẾ
+export const ROLE_IDS = {
+  member: 'ROLE_ID_MEMBER',
+  manager: 'ROLE_ID_MANAGER',
+  admin: 'ROLE_ID_ADMIN'
+};
